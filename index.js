77977c920c8a4e9ab17f05b3c72af2f432281ed5
unknown <dzulfiqarya@gmail.com>
2026-04-24 12:38:18 +0700

    Finalize navbar

--- index.js
+++ index.js
@@ -2,23 +2,21 @@ const header = document.getElementById("main-header");
 let lastScrollY = window.scrollY;
 
 window.addEventListener("scroll", () => {
-  if (!header) return; // Keamanan jika ID tidak ditemukan
   const currentScrollY = window.scrollY;
 
-  if (currentScrollY > lastScrollY && currentScrollY > 100) {
-    header.classList.add("header-hidden");
-  } else {
-    header.classList.remove("header-hidden");
-  }
-  lastScrollY = currentScrollY;
-});
-window.addEventListener("scroll", () => {
-  const header = document.querySelector(".main-header");
-  if (window.scrollY > 50) {
+  if (currentScrollY > 50) {
     header.classList.add("nav-scrolled");
   } else {
     header.classList.remove("nav-scrolled");
   }
+
+  if (currentScrollY > lastScrollY && currentScrollY > 200) {
+    header.classList.add("header-hidden");
+  } else {
+    header.classList.remove("header-hidden");
+  }
+
+  lastScrollY = currentScrollY;
 });
 const revealEls = document.querySelectorAll(".reveal");
 const revealObserver = new IntersectionObserver(
